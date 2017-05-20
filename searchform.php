b077298bebb4d12cd317dc5264d528fdb45a57ba
kodango <dangoakachan@foxmail.com>
2017-05-20 12:12:51 +0800

    remove search input value

--- searchform.php
+++ searchform.php
@@ -8,7 +8,7 @@
 <form role="search" action="/" method="get" id="searchform" class="searchform" onsubmit="location.href='<?php echo home_url('/search/'); ?>' + encodeURIComponent(this.s.value).replace(/%20/g, '+'); return false;">
     <div>
 		<label class="screen-reader-text" for="s">搜索：</label>
-		<input type="text" value="wordpress 文章" name="s" id="s" />
+		<input type="text" value="" name="s" id="s" />
  		<input type="submit" id="searchsubmit" value="搜索" />
     </div>
 </form>
